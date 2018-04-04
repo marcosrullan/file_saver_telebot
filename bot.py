2c62ee7f66636ecc622bf64efccb760ef88a8866
Marcos <marcosrullan@gmail.com>
2018-04-04 09:29:13 +0200

    Control to avoid commit the config.ini

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ files_downloaded = [line.rstrip('\n').split(" ")[1] for line in open(path_file_d
 def get_name_from_message(m):
     bigger_size = 0
     file_info = 0
-    file_name = 0
+    file_id = 0
 
     if m.content_type == 'photo':
         # we receive tree types of size from telegram api and we select the biggest size
@@ -32,18 +32,18 @@ def get_name_from_message(m):
             if photo.file_size > bigger_size:
                 bigger_size = photo.file_size
                 file_info = bot.get_file(photo.file_id)
-                file_name = photo.file_id
+                file_id = photo.file_id
     elif m.content_type == 'video_note':
         file_info = bot.get_file(m.video_note.file_id)
-        file_name = m.video_note.file_id
+        file_id = m.video_note.file_id
     elif m.content_type == 'video':
         file_info = bot.get_file(m.video.file_id)
-        file_name = m.video.file_id
+        file_id = m.video.file_id
     elif m.content_type == 'voice':
         file_info = bot.get_file(m.voice.file_id)
-        file_name = m.voice.file_id
+        file_id = m.voice.file_id
 
-    return file_name, file_info
+    return file_id, file_info
 
 
 def listener(messages):
@@ -53,8 +53,8 @@ def listener(messages):
     """
     for m in messages:
         try:
-            file_name, file_info = get_name_from_message(m)
-            if file_name not in files_downloaded:
+            file_id, file_info = get_name_from_message(m)
+            if file_id not in files_downloaded:
                 if m.content_type in ['photo', 'video_note', 'video', 'voice']:
                     # Text to know if the user want to save the file or not
                     markup = InlineKeyboardMarkup()
@@ -65,13 +65,12 @@ def listener(messages):
                                      disable_notification=True, reply_markup=markup)
             else:
                 bot.send_message(text="<b>This file has already been saved</b>", chat_id=m.chat.id,
-                                      parse_mode='HTML', reply_to_message_id=m.message_id, reply_markup=None)
+                                 parse_mode='HTML', reply_to_message_id=m.message_id, reply_markup=None)
         except Exception as e:
             print(e)
 
 
 def main():
-
     @bot.callback_query_handler(func=lambda call: True)
     def callbacks(m):
         text_to_show = ''
@@ -79,14 +78,14 @@ def main():
         # Check if the user want to saver or not
         if m.data == 'upload':
             origin_message = m.message.reply_to_message
-            file_name, file_info = get_name_from_message(origin_message)
+            file_id, file_info = get_name_from_message(origin_message)
 
             # Check if the file was downloaded previously
-            if file_name not in files_downloaded:
+            if file_id not in files_downloaded:
                 # add new file to the record
-                files_downloaded.append(file_name)
+                files_downloaded.append(file_id)
                 aux = open(path_file_downloaded, 'a')
-                aux.write('{} {}\n'.format(datetime.date.today().strftime('%Y-%m-%d'), file_name))
+                aux.write('{} {}\n'.format(datetime.date.today().strftime('%Y-%m-%d'), file_id))
                 aux.close()
 
                 downloaded_file = bot.download_file(file_info.file_path)
@@ -95,9 +94,9 @@ def main():
                 if not os.path.exists(path_to_save):
                     os.makedirs(path_to_save)
                 with open(os.path.join(path_to_save, '{} {}.{}'.format(datetime.date.today().strftime('%Y-%m-%d'),
-                                                                        file_name,
-                                                                        'jpg' if origin_message.content_type == 'photo'
-                                                                        else 'ogg')),
+                                                                       file_id,
+                                                                       'jpg' if origin_message.content_type == 'photo'
+                                                                       else 'ogg')),
                           'wb') as new_file:
                     new_file.write(downloaded_file)
 
@@ -109,7 +108,6 @@ def main():
 
     while True:
         try:
-            # If the bot has a error, with none_stop = true he never stop ?
             bot.polling(none_stop=True)
         except Exception as e:
             print(e)
